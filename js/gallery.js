aa26123e4a7a977e33756f94ee0f67ac0fc9318d
l1728 <cheburashka077@ukr.net>
2024-03-14 22:57:27 +0200

    done

--- js/gallery.js
+++ js/gallery.js
@@ -66,48 +66,39 @@ const images = [
 
 
 // Вибираємо контейнер для галереї
-const galleryContainer = document.querySelector('ul.gallery');
+const galleryContainer = document.querySelector(".gallery");
 
-
-// Генеруємо розмітку для кожного елемента галереї та додаємо до контейнера
-const galleryItemsMarkup = images
-    .map(({ preview, original, description }) => `
+// Генеруємо розмітку для кожного елемента галереї
+galleryContainer.insertAdjacentHTML("beforeend", galleryItemsMarkup(images));
+function galleryItemsMarkup(arr) {
+  return arr
+  .map(({ preview, original, description }) => `
   <li class="gallery-item">
     <a class="gallery-link" href="${original}">
       <img class="gallery-image" src="${preview}" data-source="${original}" alt="${description}">
     </a>
   </li>`)
     .join('');
+};
 
-// Додаємо розмітку елементів галереї до контейнера
-galleryContainer.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+// Додаємо обробник події для кліка на ul елементі галереї
+galleryContainer.addEventListener("click", onImageClick);
+function onImageClick(event) {
+  event.preventDefault(); // Заборона стандартної поведінки
+  const galleryItem = event.target.closest(".gallery-item"); // Знаходимо найближчий елемент з класом ".gallery-item"
+  if (galleryItem) { // Перевіряємо, чи було клікнуто на елемент галереї (елемент з класом "gallery-item")
+     const largeImageSrc = galleryItem.querySelector(".gallery-image").dataset.source; // Отримуємо посилання на велике зображення з дата-атрибуту data-source
+    console.log('Посилання на велике зображення:', largeImageSrc); // Робимо потрібні дії з посиланням на велике зображення
+  
+    // Створюємо модальне вікно з великим зображенням
+    // Підключаємо бібліотеку basicLightbox
+    const instance = basicLightbox.create(` 
+    <img src="${largeImageSrc}" width="1112" height="640">
+`)
+// Показуємо модальне вікно
+instance.show();
+  
+  }
+  };
 
 
-// Забороняє стандартну поведінку посилання (тобто завантаження зображення), коли воно клікнуте
-const galleryLinks = document.querySelectorAll('.gallery-link');
-galleryLinks.forEach(link => {  // Метод forEach використовується в даному випадку для перебору кожного посилання в колекції galleryLinks та застосовує обробник подій (функцію зворотного виклику), яка забороняє стандартну поведінку посилань при їх кліку.
-    link.addEventListener('click', function (event) {
-        event.preventDefault();
-    });
-});
-
-
-// Делегування
-// Отримаємо посилання на ul елемент галереї.
-// Додаємо обробник подій для кліка на ul елементі.
-// У функції обробника подій перевіряємо, чи було клікнуто на елемент галереї (елемент з класом "gallery-link").
-// Якщо так, ми отримуємо посилання на велике зображення з дата-атрибуту data-source та робимо потрібні дії з цим посиланням (наприклад, виводимо в консоль).
-
-// Отримуємо посилання на ul елемент галереї (вже є)
-// Додаємо обробник подій для кліка на ul елементі
-galleryContainer.addEventListener('click', function(event) {
-    // Перевіряємо, чи було клікнуто на елемент галереї (елемент з класом "gallery-link")
-    if (event.target.classList.contains("gallery-link")) {
-        // Отримуємо посилання на велике зображення з дата-атрибуту data-source
-      const largeImageSrc = event.target.dataset.sourse;
-      console.log(largeImageSrc);
-        
-        // Робимо потрібні дії з посиланням на велике зображення
-        console.log('Посилання на велике зображення:', largeImageSrc);
-    }
-});
